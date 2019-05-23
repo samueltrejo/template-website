0cb01665c235afdbd4e931cca0e09d15397aa890
Samuel T. Mendoza <samuel.mendoza@uniguest.com>
2019-05-23 15:24:19 -0500

    google auth setup

---
+++ src/javascripts/components/auth.js
@@ -0,0 +1,10 @@
+const signIn = () => {
+  const provider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth().signInWithPopup(provider);
+};
+
+const attachEvents = () => {
+  document.getElementById('sign-in').addEventListener('click', signIn);
+};
+
+export default { attachEvents };
